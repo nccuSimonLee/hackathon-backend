941fb03ebd8235bca40cf915b7ae342bfe772939
simon <simon2020291@gmail.com>
2021-05-29 14:20:09 +0800

    [add] create lineup_queue.py and add `LineupQueue` to it

---
+++ lineup_queue.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class LineupQueue(metaclass=Singleton):
+    def __init__(self):
+        self._lineup_no = 0
+        self._showup_no = 0
+        self._queue = deque()
+
+    def take_lineup_no(self):
+        lineup_no = str(self._lineup_no).zfill(3)
+        self._queue.append(lineup_no)
+        self._lineup_no += 1
+        return lineup_no
+
+    def take_showup_no(self):
+        showup_no = f'b{str(self._showup_no).zfill(3)}'
+        self._showup_no += 1
+        return showup_no
+
+    def get_next_dining_no(self):
+        return self._queue.popleft()
